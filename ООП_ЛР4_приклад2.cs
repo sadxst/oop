026c09dd2ca8af2b327c89dcfc8029e902915fd9
sadxst <dcomp96@gmail.com>
2025-04-29 00:12:53 +0300

    Приклад 2

---
+++ ООП_ЛР4_приклад2.cs
@@ -0,0 +1,39 @@
+﻿public class Rectangle
+{
+    public double A { get; set; }
+    public double B { get; set; }
+    public Rectangle(double A, double B)
+    {
+        this.A = A;
+        this.B = B;
+    }
+}
+public class RParallelepiped : Rectangle
+{
+    public double C { get; set; }
+    public RParallelepiped(double A, double B, double C) : base(A, B)
+    {
+        this.C = C;
+    }
+}
+class ООП_ЛР4_приклад2
+{
+    static void Main(string[] args)
+    {
+        double A, B, C;
+        Console.WriteLine("Enter the length of side a of the rectangle:");
+        A = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the length of side b of the rectangle:");
+        B = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the length of side c of the parallelepiped:");
+        C = double.Parse(Console.ReadLine());
+        RParallelepiped rPar = new RParallelepiped(A, B, C);
+        Console.WriteLine("\nRectangle:");
+        Console.WriteLine($"Side a: {rPar.A}");
+        Console.WriteLine($"Side b: {rPar.B}");
+        Console.WriteLine("\nParallelepiped:");
+        Console.WriteLine($"Side a: {rPar.A}");
+        Console.WriteLine($"Side b: {rPar.B}");
+        Console.WriteLine($"Side c: {rPar.C}");
+    }
+}
